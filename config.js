03c6221e4b62eb36fe8464a448d4135d795aea86
demoreh005 <demoreh005@gmail.com>
2025-05-28 00:11:28 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "nexus~lmdSSRZC#8JfUGmovXdI6ZNfocpqcYR1ZfUFVNN2z8RpvMoniGgU",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
